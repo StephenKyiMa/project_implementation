377eb770d88b6f7cfda5c6c371e9e74c6d56a7d1
StephenKyiMa <54228727+StephenKyiMa@users.noreply.github.com>
2020-11-01 17:51:53 +1030

    Update model_rnn.py

--- model_rnn.py
+++ model_rnn.py
@@ -2,7 +2,7 @@ import torchvision
 import torch.nn.functional as F 
 from config import config
 from torchvision import models
-from pretrainedmodels.models import bninception
+from torch.nn.init import kaiming_normal
 from torch import nn
 import torch
 from cnn_finetune import make_model
@@ -11,16 +11,7 @@ from config import config
 class CNN(nn.Module):
     def __init__(self):
         super(CNN, self).__init__()
-        
-        # Loading pretrained ResNet as feature extractor
-
-        self.model = make_model('resnet34', num_classes=config.num_classes, pretrained=True)
-            
-        # self.layer1 = nn.Linear(config.num_classes, embedding_size)
-
-        # for m in self.layer1:
-        #     kaiming_normal(m.weight)
-
+        self.model = make_model('resnet34', num_classes=1000, pretrained=True)
         for p in self.model.parameters():
             p.requires_grad = True
         
@@ -38,8 +29,6 @@ class DecoderRNN(nn.Module):
                             batch_first = True)
         self.classifier = nn.Linear(hidden_size, num_classes)
         self.hidden_size = hidden_size
-        
-        # Init of last layer
         kaiming_normal(self.classifier.weight)
     
 
@@ -54,25 +43,17 @@ class BuildModel(nn.Module):
         super(BuildModel, self).__init__()
         # self.cnn = CNN().cuda()
         self.cnn = CNN()
-        # self.rnn = DecoderRNN(config.num_classes, config.num_classes, 64, 10).cuda()
-        self.rnn = DecoderRNN(config.num_classes, config.num_classes, 64, 10)
+        # self.rnn = DecoderRNN(1000, 1000, 64, 10).cuda()
+        self.rnn = DecoderRNN(1000, 1000, 64, 10)
+        self.layer1 = nn.Linear(1000, config.num_classes)
     
     def forward(self, x):
         x = self.cnn(x)
-        # out = self.rnn(x)
-        # out *= x
+        out = self.rnn(x)
+        out *= x
         return x
 
 def get_net():
-    """
-    if config.channels == 4:
-        model = models.resnet34(pretrained=False)
-        model.avgpool = nn.AdaptiveAvgPool2d(1)
-        model.fc = nn.Linear(2048,config.num_classes)
-        model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3,
-                                bias=False)
-    elif config.channels == 3:
-    """
     model = BuildModel()
     return model
 
