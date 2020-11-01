600a1c5cb78f44406dcbb5441ee7ae1b4a9c396a
StephenKyiMa <54228727+StephenKyiMa@users.noreply.github.com>
2020-11-01 17:23:38 +1030

    Update model_rnn.py

--- model_rnn.py
+++ model_rnn.py
@@ -1,57 +1,14 @@
 import torchvision
 import torch.nn.functional as F 
-from torch import nn
 from config import config
-'''
-def generate_model():
-    class DenseModel(nn.Module):
-        def __init__(self, pretrained_model):
-            super(DenseModel, self).__init__()
-            self.classifier = nn.Linear(pretrained_model.classifier.in_features, config.num_classes)
-
-            for m in self.modules():
-                if isinstance(m, nn.Conv2d):
-                    nn.init.kaiming_normal(m.weight)
-                elif isinstance(m, nn.BatchNorm2d):
-                    m.weight.data.fill_(1) 0o
-                    m.bias.data.zero_()
-                elif isinstance(m, nn.Linear):
-                    m.bias.data.zero_()
-
-            self.features = pretrained_model.features
-            self.layer1 = pretrained_model.features._modules['denseblock1']
-            self.layer2 = pretrained_model.features._modules['denseblock2']
-            self.layer3 = pretrained_model.features._modules['denseblock3']
-            self.layer4 = pretrained_model.features._modules['denseblock4']
-
-        def forward(self, x):
-            features = self.features(x)
-            out = F.relu(features, inplace=True)
-            out = F.avg_pool2d(out, kernel_size=8).view(features.size(0), -1)
-            out = F.sigmoid(self.classifier(out))
-            return out
-
-    return DenseModel(torchvision.models.densenet169(pretrained=True))
-
-def get_net():
-    #return MyModel(torchvision.models.resnet101(pretrained = True))
-    model = torchvision.models.resnet50(pretrained = True)    
-    #for param in model.parameters():
-    #    param.requires_grad = False
-    model.avgpool = nn.AdaptiveAvgPool2d(1)
-    model.fc = nn.Linear(2048,config.num_classes)
-    return model
-'''
 from torchvision import models
 from pretrainedmodels.models import bninception
 from torch import nn
-from torch.nn.init import kaiming_normal
 import torch
 from cnn_finetune import make_model
 from config import config
 
 class CNN(nn.Module):
-    ## We use ResNet weights from PyCaffe.
     def __init__(self):
         super(CNN, self).__init__()
         
@@ -109,18 +66,13 @@ class BuildModel(nn.Module):
 def get_net():
     """
     if config.channels == 4:
-        model = models.resnet50(pretrained=False)
+        model = models.resnet34(pretrained=False)
         model.avgpool = nn.AdaptiveAvgPool2d(1)
         model.fc = nn.Linear(2048,config.num_classes)
         model.conv1 = nn.Conv2d(4, 64, kernel_size=7, stride=2, padding=3,
                                 bias=False)
     elif config.channels == 3:
     """
-    # model = bninception(pretrained="imagenet")
-    # model.global_pool = nn.AdaptiveAvgPool2d(1)
-    # model.conv1_7x7_s2 = nn.Conv2d(config.channels, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3))
-    # model.last_linear = nn.Linear(1024,config.num_classes)
-    # return model
     model = BuildModel()
     return model
 
